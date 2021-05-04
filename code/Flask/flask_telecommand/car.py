3d1cfbf7f507f8d55483bba092bd7d22682fe0c2
ACKERMANNGUE <gawen.ackrm@eduge.ch>
2021-05-04 15:36:12 +0200

    ajout code télécommande ajax

--- code/Flask/flask_telecommand/car.py
+++ code/Flask/flask_telecommand/car.py
@@ -1,6 +1,8 @@
+from pylgbst.comms.cgatt import GattConnection
 from pylgbst.hub import MoveHub
 from pylgbst.peripherals import Motor, EncodedMotor
 from pylgbst import *
+import os
 
 class CarController:
     """Class controlling the car
@@ -10,6 +12,7 @@ class CarController:
     MY_MOVEHUB_ADD = "90:84:2B:50:36:43"
     MY_BTCTRLR_HCI = "hci0"
     MAX_ANGLE = 150
+    DEFAULT_ANGLE = 0
     MAX_MOTOR_POWER = 1
     MOTOR_STOP_POWER = 0
     
@@ -23,11 +26,15 @@ class CarController:
                 cls.front_motor = Motor(cls.movehub, cls.movehub.PORT_A)
                 cls.back_motor = Motor(cls.movehub, cls.movehub.PORT_B)
                 cls.directionnal_motor = EncodedMotor(cls.movehub, cls.movehub.PORT_C)
+                cls.old_angle = cls.DEFAULT_ANGLE
             except:
                 cls.movehub = None
                 cls.front_motor = None
                 cls.back_motor = None
                 cls.directionnal_motor = None
+                cls.instance = None
+                cls.connection = None
+                cls.old_angle = None
         return cls.instance
     
     def move(self, motor_speed, angle_rotation):
@@ -36,7 +43,7 @@ class CarController:
         motor_speed : The motor's speed
                       NOTE : to move forward, the value must be reversed because it is basically negative
         angle_rotation : The rotation of the directionnal motor
-        """
+        """ 
         # Invert the power direction
         motor_speed *= (-1)
         # Max value of motor is -1 and +1 but in the HTML form, the range input can be set between -100 to +100
@@ -44,15 +51,18 @@ class CarController:
         
         self.front_motor.start_power(motor_speed)
         self.back_motor.start_power(motor_speed)
-        self.directionnal_motor.start_power(angle_rotation)
+        self.turn(angle_rotation)
     
 
-    def turn(self, motor, angle):
+    def turn(self, angle):
         """Turn the directionnal motor from the input value
 
-        motor : The directionnal motor
+        angle : The angle we wants the directionnal motor goes to
         """
-        motor.angled(angle)
+        
+        # Reset the angle
+        self.directionnal_motor.start_power(angle + (-1) * self.old_angle)
+        self.old_angle = angle
 
 
     def stop_moving(self):
@@ -60,5 +70,9 @@ class CarController:
         """
         self.front_motor.start_power(self.MOTOR_STOP_POWER)
         self.back_motor.start_power(self.MOTOR_STOP_POWER)
+        
+    def disconnect(self):
+        self.connection.disconnect()
+        os.system("sudo systemctl restart bluetooth.service")
         
         
--- code/Flask/flask_telecommand/telecommand.py
+++ code/Flask/flask_telecommand/telecommand.py
@@ -1,63 +1,172 @@
 
 from time import sleep
-from flask import Flask, request, render_template, Response
-from car import CarController 
+from flask import Flask, request, render_template, Response, redirect
+from flask_bootstrap import Bootstrap
+from flask_nav import Nav
+from flask_nav.elements import *
+from pylgbst.comms.cgatt import GattConnection
+from flask_caching import Cache
+from car import CarController
+
 
 DEFAULT_MODE = False
 DEFAULT_SPEED = 0
 DEFAULT_ANGLE = 0
 
-car = None
+BTN_REQUEST_VALIDATE = "validate"
+BTN_REQUEST_STOP = "stop"
+BTN_REQUEST_DISCONNECT = "disconnect"
+
+topbar = Navbar(
+    View('Accueil', 'home'),
+    View('Télécommande', 'control_car'),
+    View('Déconnexion', 'close_connection'),
+    View('Créer une connexion', 'create_car'),
+)
+
+nav = Nav()
+nav.register_element('top', topbar)
+
 
 app = Flask(__name__)
+Bootstrap(app)
+cache = Cache(config={'CACHE_TYPE': 'null'})
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = -1
+cache.init_app(app)
+
 
 @app.route('/')
 def hello():
-    return "Bonjour à vous"
+    """Route used to know if the server is on
+    """
+    return render_template("home.html")
+
+
+@app.route('/home')
+def home():
+    """Route used to know if the server is on
+    """
+    return render_template("home.html")
+
+
+@app.route('/create_car_test')
+def create_car_test():
+    """Used to test the creation of the car
+    """
+    output = "Tentative de connexion à l'appareil"
+    car = CarController()
+    if car != None:
+        if car.connection != None:
+            output = 'Tentative de connexion à l\'appareil {0} réussie'.format(
+                car.MY_MOVEHUB_ADD)
+    return render_template("connection.html", msg=output)
 
 
 @app.route('/create_car')
 def create_car():
-    car = CarController()
-    
-    return car.MY_MOVEHUB_ADD
+    """Used to the creation of the car
+    """
+    cache.clear()
+    car = None
+    while car == None:
+        car = CarController()
+    if car != None:
+        print(type(car.connection) is GattConnection)
+        if car.connection != None:
+            return render_template("form.html")
+
 
 @app.route('/close_connection')
 def close_connection():
+    """Close the connection between the Raspberry Pi and the Technic Hub
+    """
     output = "Connexion toujours en cours"
     car = CarController()
     if car.connection != None:
-        car.connection.disconnect()
-        output = "Connexion fermée"
-    return output
+        car.disconnect()
+        car.instance = None
+        output = "Connexion fermée avec l'appareil {0}".format(
+            car.MY_MOVEHUB_ADD)
+    return render_template("connection.html", msg=output)
 
 
 @app.route('/control_car')
 def control_car():
-    return render_template("form.html", mode = DEFAULT_MODE, speed = DEFAULT_SPEED, angle = DEFAULT_ANGLE)
+    """Render the form which allows to control the car
+    """
+    return render_template("form.html", mode=DEFAULT_MODE, speed=DEFAULT_SPEED, angle=DEFAULT_ANGLE)
+
+
+@app.route('/bg_processing', methods=['POST'])
+def bg_process():
+    """Process the values passed by Javascript
+    """
+    automatic_mode = DEFAULT_MODE
+    # move_speed = DEFAULT_SPEED
+    # angle_rotation = DEFAULT_ANGLE
+    # if request.form["cbxMode"] != None:
+    #     automatic_mode = request.form["cbxMode"]
+    move_speed = request.form["rngMove"]
+    angle_rotation = request.form["rngRotationAngle"]
+    car = CarController()
+    car.move(float(move_speed), int(angle_rotation))
+    return render_template("form.html", mode=automatic_mode, speed=move_speed, angle=angle_rotation)
+
 
 @app.route('/form_response', methods=['POST'])
 def form_response():
+    """The form's answer processed to execute the different actions below
+    """
+
+    # Init
     car = CarController()
     automatic_mode = DEFAULT_MODE
     move_speed = DEFAULT_SPEED
     angle_rotation = DEFAULT_ANGLE
+
+    returned_value = render_template(
+        "form.html", mode=automatic_mode, speed=move_speed, angle=angle_rotation)
+    error = render_template(
+        "error.html", msg="Une connexion est nécessaire pour pouvoir intéragir avec")
     if request.method == 'POST':
-        if request.form["send_request"] == "validate":
+        # Validate will send the values to the car
+        if request.form["send_request"] == BTN_REQUEST_VALIDATE:
             automatic_mode = request.form.get("cbxMode")
+            # form.get return a list of values and None if the checkbox isn't checked
             if request.form.get("cbxMode") != None:
                 automatic_mode = True
             move_speed = request.form["rngMove"]
             angle_rotation = request.form["rngRotationAngle"]
-            if car != None :
-                car.move(float(move_speed), int(angle_rotation))
-        elif request.form["send_request"] == "stop":
-            if car != None :
-                car.stop_moving()
-    return render_template("form.html", mode = automatic_mode, speed = move_speed, angle = angle_rotation)
-            
+            # Updates of the inputed values
+            returned_value = render_template(
+                "form.html", mode=automatic_mode, speed=move_speed, angle=angle_rotation)
+            if car != None:
+                if type(car.connection) is GattConnection:
+                    # Convert them into float values because the range are int between negative and positive 100
+                    # and the method which activate the motors is a range between negative and positive 1
+                    # so in the move methods I divide the input values by 100
+                    car.move(float(move_speed), int(angle_rotation))
+                else:
+                    returned_value = error
+        elif request.form["send_request"] == BTN_REQUEST_STOP:
+            if car != None:
+                if car.connection != None:
+                    car.stop_moving()
+                else:
+                    returned_value = error
+        elif request.form["send_request"] == BTN_REQUEST_DISCONNECT:
+            returned_value = redirect("/close_connection")
+    return returned_value
+
+
+@app.route("/error")
+def error(msg):
+    return render_template("error.html", msg=msg)
+
+
+nav.init_app(app)
+
 
 if __name__ == '__main__':
     # Lance le serveur et donne l'accès à toutes les personnes sur le réseaux
     app.run(host='0.0.0.0', debug=True)
-
